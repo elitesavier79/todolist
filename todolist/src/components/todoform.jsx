045cc9d8236ce316d1d4f5b169fb1cefedb6cffc
elitesavier79 <elitesavier79@gmail.com>
2020-05-13 12:46:24 +0700

    membuat commponen react

--- todolist/src/components/todoform.jsx
+++ todolist/src/components/todoform.jsx
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 
-const Todoform = () => {
-  return (
-    <section>
-      <form className="add">
-        <div className="add-form">
-          <input type="text" className="add-input" />
+const Todoform = ({addTodo}) => {
+  const [value, setValue] = useState("");
+
+  const handleFormSubmit = e => {
+    e.preventDefault();
+
+   if (!value) {
+     alert("no blank todo!");
+     return;
+    }
+
+    addTodo(value);
+    alert(value);
+    setValue("");
+
+  };
+
+ return (
+    <section className="add" >
+        <form className="add-form" onSubmit={handleFormSubmit}>
+          <input type="text" className="add-input" value={value} onChange={e => setValue(e.target.value)}/>
           <button className="add-btn main-black-color">Add</button>
-        </div>
-      </form>
+        </form>
     </section>
   );
+};
+
+Todoform.propTypes = {
+  addTodo : PropTypes.func.isRequired
 };
 
 export default Todoform;
