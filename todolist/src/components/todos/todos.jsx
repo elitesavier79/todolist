b8a750bed14ca54f6ad928d5b7af0b51139aa49d
elitesavier79 <elitesavier79@gmail.com>
2020-05-28 13:14:45 +0700

    membuat emotion css props dan theming di todos component

--- todolist/src/components/todos/todos.jsx
+++ todolist/src/components/todos/todos.jsx
@@ -1,12 +1,16 @@
-import React from "react";
+/** @jsx jsx */
+import { jsx } from "@emotion/core";
+//import React from "react";
+
 import PropTypes from "prop-types";
 import Todo from "../todo/todo";
 
-import styles from "./todos.module.css";
+//import styles from "./todos.module.css";
+import * as styles from "./todos.styles";
 
 const Todos = ({ todos, completeTodo }) => {
   return (
-    <section className={styles.todos}>
+    /*<section className={styles.todos}>
       {todos.length > 0 &&
         todos.map((todo, index) => {
           return (
@@ -22,6 +26,27 @@ const Todos = ({ todos, completeTodo }) => {
       {todos.length === 0 && (
         <div className={styles.todosText}>
           Silakan mengklik tombol <span className={styles.spanText}>add</span>{" "}
+          di atas untuk memulai Todo
+        </div>
+      )}
+    </section>*/
+
+    <section css={styles.todos}>
+      {todos.length > 0 &&
+        todos.map((todo, index) => {
+          return (
+            <Todo
+              key={index}
+              text={todo.text}
+              isCompleted={todo.isCompleted}
+              completeTodo={completeTodo}
+              index={index}
+            />
+          );
+        })}
+      {todos.length === 0 && (
+        <div css={styles.todosText}>
+          Silakan mengklik tombol <span css={styles.spanText}>add</span>{" "}
           di atas untuk memulai Todo
         </div>
       )}
---
+++ todolist/src/components/todos/todos.styles.js
@@ -0,0 +1,23 @@
+import { css } from '@emotion/core';
+
+export const todos = ( ) =>
+	css`
+		display: flex;
+  		flex-direction: column;
+  		min-height: 400px;
+	`;
+
+export const todosText = () =>
+	css`
+		font-family: "homemade Apple", sans-serif;
+  		font-size: 24px;
+  		text-align: center;
+  		padding: 16px;
+	`;
+
+export const spanText = () =>
+	css`
+		font-family: "bungee", sans-serif;
+  		font-size: 24px;
+  		color: orange;
+	`;
