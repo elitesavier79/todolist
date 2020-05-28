f10468996d4fa832e02c28fe0ab822459503a904
elitesavier79 <elitesavier79@gmail.com>
2020-05-28 12:48:29 +0700

    membuat emotion css props dan theming di todoform component

---
+++ todolist/src/components/todoform/todoform.styles.js
@@ -0,0 +1,43 @@
+import { css } from '@emotion/core';
+
+export const add = ( ) =>
+	css`
+		padding: 16px;
+	`;
+
+export const addForm = () =>
+	css`
+		display: flex;
+  		align-items: flex-start;
+	`;
+
+export const addInput = ({ theme }) =>
+	css`
+		background: unset;
+  		border: unset;
+  		padding: 0 64px;
+  		flex: 1;
+  		border-bottom: 1px solid ${theme.color.primary.red};
+  		ouline: unset;
+  		font-family: "homemade Apple", sans-serif;
+  		font-size: 16px;
+  		text-transform: lowercase;
+	`;
+
+export const addBtn = ({ theme }) =>
+	css`
+		font-size: 1.8rem;
+  		font-family: "bungee", sans-serif;
+  		padding: 16px;
+ 		padding-right: unset;
+ 		cursor: pointer;
+ 	 	background: unset;
+  		background: unset;
+  		border: unset;
+  		outline: unset;
+  		&:active {
+  			text-shadow: 1px 1px 2px ${theme.color.primary.black};
+  		}
+
+	`; 
+
